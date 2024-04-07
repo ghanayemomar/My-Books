7e229e5b16b39f0e0a85defd82dbd2eedfb880f1
ghanayemomar <omar.mohd.ghanayem@gmail.com>
2024-04-08 01:10:02 +0300

    10 - Adding Your EF Core DB Context

---
+++ my-books/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+             
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }  
+    }
+}
--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string ConnectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 
+// Configure DBContext with SQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
