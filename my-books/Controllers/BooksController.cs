4b522897418ac6524c390c9ae30194cb0d75e248
ghanayemomar <omar.mohd.ghanayem@gmail.com>
2024-04-11 20:28:15 +0300

    15 - Listing All Books [HttpGet]

--- my-books/Controllers/BooksController.cs
+++ my-books/Controllers/BooksController.cs
@@ -10,15 +10,31 @@ namespace my_books.Controllers
     public class BooksController : ControllerBase
     {
         public BooksService _booksService;
+
         public BooksController(BooksService booksService)
         {
             _booksService = booksService;
         }
+
         [HttpPost("add-book")]
         public IActionResult AddBook([FromBody] BookVM book)
         {
             _booksService.AddBook(book);
             return Ok();
+        }
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
         }
     }
 }
--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -30,5 +30,14 @@ namespace my_books.Data.Services
             _context.SaveChanges();
         }
 
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+        public Book GetBookById(int bookId)
+        {
+            return _context.Books.FirstOrDefault(x => x.Id == bookId);
+        }
+
     }
 }
